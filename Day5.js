d23ca65cccbb961233c61bacd19d269423ce0539
kolluvinay425 <kolluvinay425@gmail.com>
2021-06-25 16:55:26 +0200

    testcompleted

--- Day5.js
+++ Day5.js
@@ -9,60 +9,119 @@ ASSIGNMENT RULES
 /* EXERCISE 1
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
-function area(11,12){
-    areaRectangle = 
+const area = function (a,b) {
+
+    areaRectangle = a*b
+    return areaRectangle
+    
 }
+result = area(11,12)
+console.log('this is ex1 result: ',result)
 
 /* EXERCISE 2
 Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
-
+creazySum = function(a,b){
+    return a+b
+}
+sum = creazySum(20,30)
+console.log('this one is exercise 2 result : ',sum)
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
 It should return triple their absolute difference if the given number is greater than 19.
 */
+creazyDiff = function (){
 
+}
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
-
+boundary = function (n){
+    if(n>20 && n<=100 || n==400){
+        return true
+    }else{
+        return false
+    }
+}
+boundaryResult = boundary(400)
+console.log('this is exercise 4 result :',boundaryResult)
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 5
 Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
-
+strivify = function (string){
+    if(string == 'Strive'){
+        result = string
+        return result
+    }
+    return string + 'Strive'   
+}
+resultStrivify = strivify('test')
+console.log('exercise 5 result: ',resultStrivify)
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
+check3and7 = function (number){
+    if(number%3 ==0 || number%7 ==0){
+        return true
+    }else{
+        return false
+    }
+}
+check3and7Result = check3and7(30)
+console.log('exercise 6 result: ',check3and7Result)
 
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
 */
+reverseString = function(string){
+    result = string.split('').reverse().join('')
+    return result
+}
 
+reverseStringResult = reverseString('Strive')
+console.log('exercise 7 result :',reverseStringResult)
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
 */
-
+upperFirst = function(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+}
+console.log('exercise 8 solution :',upperFirst('vinay'))
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
-
+cutString = function(string){
+    result = string.substring(1, string.length - 1);
+    //result2 = result.length() - 1
+    return result
+}
+console.log('exercise 9 result ',cutString('Strive'))
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
+giveMeRandom = function(n){
+
+    let array = []
+    for(i = 0; i < n.length; i++) {
+        return array.push(Math.floor(Math.random() * 10))
+    }
+    return array;
+}
+console.log('exercise 10 result: ',giveMeRandom(9))
 
 /* WRITE YOUR CODE HERE */
 
